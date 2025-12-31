fad648eee54c9866c2194a3da77e73574dc774e2
judeboy <judeeboyy@gmail.com>
2025-12-31 18:16:07 -0500

    add IAC

---
+++ sst.config.ts
@@ -0,0 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path='./.sst/platform/config.d.ts' />
+
+const domain = 'blurbbles.com'
+
+export default $config({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app(input) {
+    return {
+      name: 'blurbbles-web',
+      removal: 'remove',
+      home: 'aws',
+    };
+  },
+  async run() {
+    const { stage } = $app
+    const environment = {
+      STAGE: stage,
+      LOG_LEVEL: process.env.LOG_LEVEL as string
+    }
+
+    const web = new sst.aws.Nextjs("blurbbles-nextjs", {
+      server: {
+        architecture: 'arm64',
+        timeout: '10 minutes'
+      },
+      domain: {
+        name: domain,
+        redirects: [`www.${domain}`]
+      },
+      environment
+    });
+
+    return {
+      url: web.url
+    }
+  },
+});
