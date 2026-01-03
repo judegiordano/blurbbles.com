e62a2019d80cc81fcd742070cffa16d1f54fc091
judeboy <judeeboyy@gmail.com>
2026-01-03 09:58:31 -0500

    change cache

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Countdown } from '@/components/Countdown';
 import { type IPrompt } from '@/types/prompt';
 
 export default async function Home() {
-  const response = await fetch('https://api.blurbbles.com/prompts/daily', { next: { revalidate: 60 } })
+  const response = await fetch('https://api.blurbbles.com/prompts/daily', { cache: 'no-cache', next: { revalidate: 60 } })
   const prompt: IPrompt = await response.json()
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
