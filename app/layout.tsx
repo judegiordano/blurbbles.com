948c9d0dce03e036ca2b249342af248d6505a34a
judeboy <judeeboyy@gmail.com>
2026-01-04 12:28:54 -0500

    proxy unami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import "../styles/fade-in.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="retro">
+      <Script src="/stats.js" data-website-id="327d085b-aa9c-4fa5-9d7c-b8e65eb244ff" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/stats.js',
+        destination: 'https://cloud.umami.is/script.js',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
